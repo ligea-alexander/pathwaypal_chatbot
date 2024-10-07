ad827590016c76c72bed49865acb8375fac1b862
Ligea Alexander <49163402+ligea-alexander@users.noreply.github.com>
2024-10-07 03:01:05 +0000

    Adding logging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from pathway_pal import rag  # Make sure this function is importable
+import logging
+
+# Configure logging only once; choose the format you prefer
+logging.basicConfig(filename='chatbot_logs.log', level=logging.INFO,
+                    format='%(asctime)s - USER: %(user)s - QUERY: %(query)s - ANSWER: %(answer)s')
+
+def log_interaction(query, answer, user="test_user"):
+    # Log the user query and the chatbot's response
+    logging.info('USER: %s - QUERY: %s - ANSWER: %s', user, query, answer)
 
 def main():
     st.title("PathwayPal Chatbot")
@@ -12,6 +21,9 @@ def main():
             # Use the rag function from pathway_pal.py to get the answer
             answer = rag(query)
             st.write("Answer:", answer)
+
+            # Log the query and the answer
+            log_interaction(query, answer)
         else:
             st.error("Please enter a query to get an answer.")
 
